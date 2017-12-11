86655306b31fd9cf3c52877692711decf3a076cd
Ox24 <timur.tasci@isw.uni-stuttgart.de>
2017-12-11 13:29:18 +0100

    updated setup to new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ITArchLib',
-    version='0.1dev',
+    version='0.2dev',
     packages=['lib',],
     license='MIT',
     long_description=open('README.md').read(),
