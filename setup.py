aaed251843f682a4c779615850fc16b736150a17
Martin Wolf <32660621+wolfmar@users.noreply.github.com>
2019-02-20 08:11:48 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ITArchLib',
-    version='0.2.1dev',
+    version='0.3.0',
     packages=['lib',],
     license='MIT',
     long_description=open('README.md').read(),
