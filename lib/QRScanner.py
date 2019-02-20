6162f9f4f9130b1627af53cfb35f17e0ca2a19f9
Martin Wolf <32660621+wolfmar@users.noreply.github.com>
2019-02-20 08:11:43 +0100

    ditch zbar for pyzbar to support python3

--- lib/QRScanner.py
+++ lib/QRScanner.py
@@ -1,12 +1,9 @@
 """ provides functions to extract, read, and compare qr codes
 """
 
-
-
 import numpy as np
 import cv2
-import zbar
-from PIL import Image
+import pyzbar.pyzbar as pyzbar
 
 
 def crop_qr_code(frame):
@@ -17,24 +14,22 @@ def crop_qr_code(frame):
     :return: cropped and binarized qr code
     :rtype: numpy.ndarray
     """
+    barcode = pyzbar.decode(frame, symbols=[pyzbar.ZBarSymbol.QRCODE])
+    retval = None
+    if barcode:
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = frame[:, :, 0]
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Uses PIL to convert the grayscale image into a ndary array that ZBar can understand.
-    image = Image.fromarray(gray)
-    width, height = image.size
-    zbar_image = zbar.Image(width, height, 'Y800', image.tostring())
-    # Scans the zbar image.
-    scanner = zbar.ImageScanner()
-    scanner.scan(zbar_image)
-
-    for decoded in zbar_image:
-        qpoints = np.asarray(decoded.location)
+        # Only check first recognized barcode
+        qpoints = np.asarray(decode_results[0].polygon)
         pts1 = np.float32([qpoints[0], qpoints[3], qpoints[1], qpoints[2]])
         pts2 = np.float32([[0, 0], [300, 0], [0, 300], [300, 300]])
         M = cv2.getPerspectiveTransform(pts1, pts2)
         dst = cv2.warpPerspective(gray, M, (300, 300))
         ret3, th = cv2.threshold(dst, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        return th
+        retval = th
+
+    return retval
 
 
 def read_qr_code(qrcode_frame):
@@ -46,14 +41,17 @@ def read_qr_code(qrcode_frame):
     :rtype: String
     """
 
-    image = Image.fromarray(qrcode_frame)
-    width, height = image.size
-    zbar_image = zbar.Image(width, height, 'Y800', image.tostring())
-    scanner = zbar.ImageScanner()
-    scanner.scan(zbar_image)
+    barcode = pyzbar.decode(qrcode_frame)
+    retval = None
 
-    for decoded in zbar_image:
-        return decoded.data
+    if barcode:
+        # Handle encoding errors: https://sourceforge.net/p/zbar/discussion/664596/thread/ed7aca9d/#e9bf
+        try:
+            retval = barcode[0].data.decode("ascii")
+        except UnicodeDecodeError:
+            retval = barcode[0].data.decode("utf-8").encode("sjis").decode('utf-8')
+
+    return retval
 
 
 def qr_codes_equal(qr1, qr2):
@@ -68,31 +66,10 @@ def qr_codes_equal(qr1, qr2):
     if qr2 is None:
         return False
     else:
-        image = Image.fromarray(qr1)
-        width, height = image.size
-        zbar_image = zbar.Image(width, height, 'Y800', image.tostring())
+        string1 = read_qr_code(qr1)
+        string2 = read_qr_code(qr2)
 
-        # Scans the zbar image.
-        scanner = zbar.ImageScanner()
-        scanner.scan(zbar_image)
-
-        string1 = 'a'
-        for decoded in zbar_image:
-            string1 = decoded.data
-
-        image = Image.fromarray(qr2)
-        width, height = image.size
-        zbar_image = zbar.Image(width, height, 'Y800', image.tostring())
-
-        # Scans the zbar image.
-        scanner = zbar.ImageScanner()
-        scanner.scan(zbar_image)
-
-        string2 = 'b'
-        for decoded in zbar_image:
-            string2 = decoded.data
-
-        if string1.strip() == string2.strip():
+        if string1 and string2 and string1.strip() == string2.strip():
             return True
         else:
             return False
